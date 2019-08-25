812f6b42404236660ff08a1108dcc8fadd70c5c9
Thiago Ruis <thiago.ruis@stormtech.com.br>
2019-08-25 16:47:17 -0300

    Fixed appName at server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const appName = 'ruis-hello-world-angular';
+const appName = 'hello-world-angular';
 
 // Static file serving
 app.use(express.static(__dirname + '/dist/' + appName));
