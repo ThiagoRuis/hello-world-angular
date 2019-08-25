46e02ed94ae54979a7b9c5d385788cb2fd2a5919
Thiago Ruis <thiago.ruis@stormtech.com.br>
2019-08-25 16:05:27 -0300

    Adding express to serve the frontend in heroku

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const appName = 'ruis-hello-world-angular';
+
+// Static file serving
+app.use(express.static(__dirname + '/dist/' + appName));
+
+// Request router
+app.get('/*', function(req, res){
+    res.sendFile(path.join(__dirname + '/dist/' + appName + '/index.html'));
+});
+
+// Heroku port
+app.listen(process.env.PORT || 3000);
